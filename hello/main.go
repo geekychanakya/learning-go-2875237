b3d270ae4015410137f4799a5f8d500e2b7163b8
geekychanakya <arnab10saha@gmail.com>
2024-04-14 21:45:01 +0530

    add first Hello main go file

---
+++ hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+// help from https://stackoverflow.com/questions/67306638/go-test-results-in-go-cannot-find-main-module-but-found-git-config-in-users
+
+func main() {
+	fmt.Println("Hello from Go!")
+}
